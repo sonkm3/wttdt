82f3df493b4b28693d5af5ff0ef974749e22a85b
sonkm3 <sonkm3@gmail.com>
2016-06-03 18:46:26 +0900

    python3x

--- lib/common/errorhandler.py
+++ lib/common/errorhandler.py
@@ -8,6 +8,6 @@ class ErrorHandler(object):
         sys.stderr.write("----------------------------\n")
         sys.stderr.write(sys.exc_info()[1].__class__.__name__ + "\n")
         sys.stderr.write("----------------------------\n")
-        sys.stderr.write(traceback.format_exc().encode("utf-8"))
+        sys.stderr.write(traceback.format_exc())
         sys.stderr.write("\n")
         sys.stderr.write("----------------------------\n")
