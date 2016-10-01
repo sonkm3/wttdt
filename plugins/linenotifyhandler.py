1edac3ec31065b14c9fc312497b51fc262d7db34
sousuke nakamura <sonkm3@gmail.com>
2016-10-01 19:32:03 +0900

    restrict media type to photo. temporary log function.

--- plugins/linenotifyhandler.py
+++ plugins/linenotifyhandler.py
@@ -27,22 +27,30 @@ class LinenotifyHandler(StatusHandlerAbstract):
         self.send_notify(key, self.format_message(data))
         if 'extended_entities' in data and 'media' in data['extended_entities']:
             for count, media in enumerate(data['extended_entities']['media']):
-                self.send_notify(key, '%d/%d' % (count, len(data['extended_entities']['media'])), media['media_url'])
+                self.send_notify(key, "%d/%d\n%s" % (count, len(data['extended_entities']['media']),media['media_url']), media)
         return
 
     def format_message(self, data):
         text = data['full_text'] if data['truncated'] == True else data['text']
         return "%s\n%s" % (text, data['user']['screen_name'])
 
-    def send_notify(self, key, message, image=None):
+    def send_notify(self, key, message, media=None):
         url = 'https://notify-api.line.me/api/notify'
         payload = {'message': message}
         headers = {'Authorization':  'Bearer %s' % key}
 
-        if image:
-            payload['imageThumbnail'] = image
-            payload['imageFullsize'] = image + ':orig'
+        if media and media['type'] == 'photo':
+            payload['imageThumbnail'] = media['media_url']
+            payload['imageFullsize'] = media['media_url'] + ':orig'
+
+        self.log('payload: '+ str(payload))
+        self.log('key: '+ str(key))
 
         r = requests.post(url, data=payload, headers=headers)
 
+        self.log('status: '+ str(r.status_code))
+
         return r.status_code
+
+    def log(self, message):
+        print(message)
