925efa8b52129d0101203129095053bd436a1417
sousuke nakamura <sonkm3@gmail.com>
2016-10-01 19:12:38 +0900

    workaround...

--- lib/handler/echohandler.py
+++ lib/handler/echohandler.py
@@ -3,12 +3,20 @@ import json
 from lib.common.statushandlerabstract import StatusHandlerAbstract
 
 class EchoHandler(StatusHandlerAbstract):
+
+    def __init__(self, jsonlike=False):
+        self.jsonlike = jsonlike
+
     def handle(self, each_response):
-        if each_response.rstrip() == "":
-            print ('blank')
+        if each_response.decode('utf-8').strip() == "":
+            print ('blank,')
             return StatusHandlerAbstract.handle(self, each_response)
         else:
-            print (each_response.rstrip())
+            each_line = each_response.decode('utf-8').strip()
+            if(each_line.isprintable() and not each_line.isspace()):
+                print (each_line)
+                if(self.jsonlike):
+                    print(',')
             return each_response
 #        data = json.loads(each_response)
 #        if u'user' in data and u'text' in data:
