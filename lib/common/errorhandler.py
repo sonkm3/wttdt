7789ed2ef6d836fa9c1c72c3c3db38046bcc0b45
sousuke nakamura <sonkm3@gmail.com>
2016-10-05 16:39:24 +0900

    show additional information when handling exceptions.

--- lib/common/errorhandler.py
+++ lib/common/errorhandler.py
@@ -8,6 +8,8 @@ class ErrorHandler(object):
         sys.stderr.write("----------------------------\n")
         sys.stderr.write(sys.exc_info()[1].__class__.__name__ + "\n")
         sys.stderr.write("----------------------------\n")
+        sys.stderr.write(str(sys.exc_info()[1].__dict__) + "\n")
+        sys.stderr.write("----------------------------\n")
         sys.stderr.write(traceback.format_exc())
         sys.stderr.write("\n")
         sys.stderr.write("----------------------------\n")
